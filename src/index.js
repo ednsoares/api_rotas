36a9378b020b5ff95da6706cf4709e313a9aa99f
Ednei Soares <118008288+ednsoares@users.noreply.github.com>
2024-02-17 15:06:38 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import express from "express"
 
 import conectaDB from "./database/db.js"
 import routes from "./routes.js"
-
+const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
 conectaDB().then(() => {
-    app.listen(3000,() => console.log("Servidor e Banco de dados Operando"))
+    app.listen(port,() => console.log("Servidor e Banco de dados Operando"))
 }).catch((error) => console.log("error"))
